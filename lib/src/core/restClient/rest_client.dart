71043b17e2c804d68ea4237597b4e5eb9222816a
unknown <ricknogueira1231@gmail.com>
2023-08-18 19:25:17 -0300

    spec

---
+++ lib/src/core/restClient/rest_client.dart
@@ -0,0 +1,26 @@
+import 'package:dio/dio.dart';
+import 'package:dio/io.dart';
+
+class RestClient extends DioForNative {
+  // TODO: MODIFICAR O IP
+  RestClient()
+      : super(BaseOptions(
+          baseUrl: 'http://192.168.141.166:8080',
+          connectTimeout: const Duration(seconds: 10),
+          receiveTimeout: const Duration(seconds: 60),
+        )) {
+    interceptors.addAll([
+      LogInterceptor(requestBody: true, responseBody: true),
+    ]);
+  }
+
+  RestClient get auth {
+    options.extra['DIO_AUTH_KEY'] = true;
+    return this;
+  }
+
+  RestClient get unAuth {
+    options.extra['DIO_AUTH_KEY'] = false;
+    return this;
+  }
+}
